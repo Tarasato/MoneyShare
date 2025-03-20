c0ffd78016008809dd158ead94d9e41631302540
Tarasato <taramiratsu@gmail.com>
2025-03-20 11:38:42 +0700

    V.1.0

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, } from 'react-native';
+import MoneyShare from './MoneyShare';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaProvider >
+      <SafeAreaView style={styles.container}>
+      <MoneyShare />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+    </SafeAreaProvider>
+
   );
 }
 
@@ -15,6 +20,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 35,
   },
 });
---
+++ MoneyShare.tsx
@@ -0,0 +1,110 @@
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import React from 'react'
+import taiga from './assets/TaigaIcon.png';
+import Checkbox from 'expo-checkbox';
+import { useState } from 'react';
+
+const MoneyShare = () => {
+    const [money, setMoney] = useState('');
+    const [people, setPeople] = useState('');
+    const [tip, setTip] = useState(false);
+    const [tipValue, setTipValue] = useState('');
+    const [moneyShare, setMoneyShare] = useState("0.00");
+
+    const calculate = () => {
+        if (money == '' ) {
+            alert('กรุณาป้อนข้อมูลจำนวนเงิน');
+            return;
+        }else if(people == ''){
+            alert('กรุณาป้อนข้อมูลจำนวนคน');
+            return;
+        }else if(tip == true && tipValue == ''){
+            alert('กรุณาป้อนข้อมูลจำนวนเงินทิป');
+            return;
+        }
+        setMoneyShare(((parseFloat(money) + (tip ? parseFloat(tipValue) : 0)) / parseFloat(people)).toFixed(2).toString());
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={{ width: '100%', alignItems: 'center' }}>
+                <View style={styles.appbar}>
+                    <Text style={styles.titleAppbar}>MoneyShare</Text>
+                </View>
+                <Image source={taiga} style={styles.logo}></Image>
+
+                <TextInput keyboardType='numeric' placeholder='ป้อนจำนวนเงิน' style={styles.inputNumber} value={money} onChangeText={setMoney} ></TextInput>
+
+                <TextInput keyboardType='numeric' placeholder='ป้อนจำนวนคน' style={styles.inputNumber} value={people} onChangeText={setPeople} ></TextInput>
+                <View style={{ height: 30 }} />
+                <View style={{ flexDirection: 'row' }}>
+                    <Checkbox value={tip} onValueChange={() => {
+                        setTip(!tip)
+                        if (tip == true) {
+                            setTipValue('');
+                        }
+                    }} />
+                    <View style={{ width: 5 }} />
+                    <Text>ทิปให้พนักงาน</Text>
+                </View>
+                <TextInput keyboardType='numeric' placeholder='ป้อนจำนวนเงินทิป' style={styles.inputNumber} value={tipValue} onChangeText={setTipValue} editable={tip} ></TextInput>
+                <TouchableOpacity style={styles.btnCal} onPress={() => calculate()}>
+                    <Text style={{ color: 'white', textAlign: 'center' }} >คำนวณ</Text>
+                </TouchableOpacity>
+                <View style={{ height: 30 }} />
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.result}>หารกันคนละ</Text>
+                    <Text style={styles.result}> {moneyShare} </Text>
+                    <Text style={styles.result}>บาท</Text>
+                </View>
+            </View>
+        </TouchableWithoutFeedback>
+    )
+}
+
+export default MoneyShare
+
+const styles = StyleSheet.create({
+    appbar: {
+        flexDirection: "row",
+        backgroundColor: "red",
+        alignItems: "center",
+        justifyContent: "center",
+        height: 50,
+        width: "100%",
+        borderBottomWidth: 1,
+        borderBottomColor: "black",
+    },
+    titleAppbar: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    logo: {
+        width: 150,
+        height: 165,
+        marginBottom: 50
+    },
+    inputNumber: {
+        width: '80%',
+        height: 50,
+        borderWidth: 1,
+        marginTop: 20,
+        borderRadius: 5,
+        padding: 10
+    },
+    btnCal: {
+        backgroundColor: '#22AA59',
+        width: '80%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        marginTop: 20
+    },
+    result: {
+        color: 'red',
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+})
